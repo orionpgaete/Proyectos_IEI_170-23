297948a29c846867401370c6029965ef36df58a8
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-10-31 12:39:44 -0300

    validaciones formulario

--- Proyecto_5/proyecto_5_app/forms.py
+++ Proyecto_5/proyecto_5_app/forms.py
@@ -1,8 +1,42 @@
 # forms.py
 
 from django import forms
+from django.core import validators 
 
 class registrousuario(forms.Form):
+
+    ESTADOS = [('activo', 'ACTIVO'), ('inactivo', 'INACTIVO'), ('bloquedo', 'BLOQUEADO')]
+
     nombre = forms.CharField()
-    fono = forms.CharField()
+    fono = forms.CharField(required=False)
     email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    estado = forms.CharField(widget=forms.Select(choices=ESTADOS))
+
+  #  def clean_nombre(self):
+     #   inputnombre = self.cleaned_data['nombre']
+     #   if inputnombre == 'Pablo':
+   #         raise forms.ValidationError("No se aceptan mas PABLO!!!")
+  #      return inputnombre
+#
+    def clean_nombre(self):
+        inputnombre = self.cleaned_data['nombre']
+        if len(inputnombre) > 20:
+            raise forms.ValidationError("El largo maximo del nombre son 20 caracteres...")
+        return inputnombre
+    
+    def clean_email(self):
+        inputmail = self.cleaned_data['email']
+        if inputmail.find('@') == -1:
+            raise forms.ValidationError("El correo debe contener un @")
+        return inputmail
+
+
+
+
+    nombre.widget.attrs['class'] = 'form-control'
+    fono.widget.attrs['class'] = 'form-control'
+    email.widget.attrs['class'] = 'form-control'
+    password.widget.attrs['class'] = 'form-control'
+    estado.widget.attrs['class'] = 'form-select'
+    
--- Proyecto_5/proyecto_5_app/views.py
+++ Proyecto_5/proyecto_5_app/views.py
@@ -4,5 +4,13 @@ from . import forms
 
 def index(request):
     form = forms.registrousuario()
+    if request.method == 'POST':
+        form = forms.registrousuario(request.POST)
+        if form.is_valid():
+            print("FORMULARIO OK!!!")
+            print("Nombre: ", form.cleaned_data['nombre'])
+            print("Email: ", form.cleaned_data['email'])
+            print("Fono: ", form.cleaned_data['fono'])
+            
     data = {'forms': form}
     return render(request, 'index.html', data)
